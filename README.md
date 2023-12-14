45d56c24ca97781f976f88b948ae18f4e693f676
RoryEiffe <roryeiffe8@gmail.com>
2023-12-14 12:25:40 -0500

    Added lab, main, and tests for this lab. Going to checkout and test on my own

---
+++ README.md
@@ -0,0 +1,16 @@
+# Transforming
+
+This lab will utilize LangChain to transform documents. The main method of transforming documents will be splitting the contents into more manageable pieces. 
+
+## Instructions
+- The provided code for this lab contains some starter code to introduce the idea of transforming documents. You should read the pre-written code to understand the overall idea of document transformation. Once this is understood, look out for TODO statements and make the appropriate changes.
+
+## Extra Reading
+- Take a look at the official [Python Langchain Docs](https://python.langchain.com/docs/expression_language/get_started)
+
+## Shouldn't Modify (But Look at for Context)
+- [main/app.py](main/app.py) (Can also run this for manual testing)
+- [test/test.py](test/test.py)
+
+## Should Modify
+- [main/lab.py](main/lab.py) - look out for TODO statements
---
+++ src/main/test/test.py
@@ -0,0 +1,33 @@
+import unittest
+from main.lab import get_html_headers, get_md_headers, transform_txt
+
+class TestTransform(unittest.TestCase):
+
+    def test_html_headers(self):
+        headers = get_html_headers()
+        L = []
+        for header in headers:
+            self.assertIsInstance(header, tuple)
+            L.append(header[0])
+        self.assertIn("h1", L)
+        self.assertIn("h2", L)
+    
+    def test_md_headers(self):
+        headers = get_md_headers()
+        L = []
+        for header in headers:
+            self.assertIsInstance(header, tuple)
+            L.append(header[0])
+        self.assertIn("##", L)
+        self.assertIn("###", L)
+        self.assertIn("####", L)
+        self.assertIn("#####", L)
+    
+    def test_text_splitter(self):
+        split_text = transform_txt("resources/sample.txt")
+        self.assertIsInstance(split_text, list)
+        self.assertGreater(len(split_text), 1)
+        self.assertLess(len(split_text), 10)
+
+if __name__ == '__main__':
+    unittest.main()
